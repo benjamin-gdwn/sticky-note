48e669dbab15a47b5d953540f943519b07754c2c
Ben_G <ben.goodwin.work1@gmail.com>
2022-06-29 16:55:03 +0100

    Commit to main branch with all files

---
+++ build/precache-manifest.039b058319fff1fcf892828cfd7db38f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f877079456d232c8cfd8",
+    "url": "/sticky-note/static/css/main.4d8f045d.chunk.css"
+  },
+  {
+    "revision": "f877079456d232c8cfd8",
+    "url": "/sticky-note/static/js/main.88f403e2.chunk.js"
+  },
+  {
+    "revision": "851c8ab8dd2793e20ba2",
+    "url": "/sticky-note/static/js/runtime~main.d5e6da27.js"
+  },
+  {
+    "revision": "59fa5faa0d43ca05c280",
+    "url": "/sticky-note/static/js/2.da757658.chunk.js"
+  },
+  {
+    "revision": "ee175d9ec2ed897b03f0378b39a5d1ee",
+    "url": "/sticky-note/index.html"
+  }
+];
