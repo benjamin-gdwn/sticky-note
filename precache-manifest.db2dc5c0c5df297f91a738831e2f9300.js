a342926663f0505daf35bc1d8f62e5600c1b183a
Ben_G <ben.goodwin.work1@gmail.com>
2022-06-29 17:02:45 +0100

    Updates

---
+++ precache-manifest.db2dc5c0c5df297f91a738831e2f9300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa915296818973f1e3f",
+    "url": "/sticky-note/static/css/main.ec326843.chunk.css"
+  },
+  {
+    "revision": "aaa915296818973f1e3f",
+    "url": "/sticky-note/static/js/main.88f403e2.chunk.js"
+  },
+  {
+    "revision": "851c8ab8dd2793e20ba2",
+    "url": "/sticky-note/static/js/runtime~main.d5e6da27.js"
+  },
+  {
+    "revision": "c00460a873d9951514dc",
+    "url": "/sticky-note/static/js/2.5a0ac17f.chunk.js"
+  },
+  {
+    "revision": "80fadc14ad85d8e6178fc1f28d93b63f",
+    "url": "/sticky-note/index.html"
+  }
+];
